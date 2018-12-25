8de662709d1d8423fbcb155a4a694a482fdf6cc1
iudragon <42891947+TeamLewok@users.noreply.github.com>
2018-12-25 17:45:10 +0530

    10 Video. RecyclerView Setup HomeFragment

--- app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/ui/HomeFragment.java
+++ app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/ui/HomeFragment.java
@@ -2,10 +2,17 @@ package dragon.bakuman.iu.spotifyscratchaudiostreammitch.ui;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 import dragon.bakuman.iu.spotifyscratchaudiostreammitch.R;
 import dragon.bakuman.iu.spotifyscratchaudiostreammitch.adapters.HomeRecyclerAdapter;
@@ -17,9 +24,11 @@ public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeS
 
 
     // UI Components
-
+    private RecyclerView mRecyclerView;
 
     // Vars
+    private HomeRecyclerAdapter mAdapter;
+    private ArrayList<String> mCategories = new ArrayList<>();
 
 
     @Override
@@ -30,7 +39,27 @@ public class HomeFragment extends Fragment implements HomeRecyclerAdapter.IHomeS
 
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        initRecyclerView(view);
+    }
+
+    private void initRecyclerView(View view) {
+
+        if (mRecyclerView == null) {
+
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new HomeRecyclerAdapter(mCategories, getActivity(), this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+
+    }
+
+    @Override
     public void onCategorySelected(int position) {
+
+        Log.d(TAG, "onCategorySelected: list item is clicked");
 
     }
 }
