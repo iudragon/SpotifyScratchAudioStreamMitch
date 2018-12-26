459d821495b926d8854b272bfd05d300508b703f
iudragon <42891947+TeamLewok@users.noreply.github.com>
2018-12-26 16:16:00 +0530

    16 Video. recycler view setup for playlist fragment

--- app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/models/Artist.java
+++ app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/models/Artist.java
@@ -1,6 +1,9 @@
 package dragon.bakuman.iu.spotifyscratchaudiostreammitch.models;
 
-public class Artist  {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Artist implements Parcelable {
 
     private String title;
     private String image;
@@ -14,6 +17,24 @@ public class Artist  {
 
     public Artist() {
     }
+
+    protected Artist(Parcel in) {
+        title = in.readString();
+        image = in.readString();
+        artist_id = in.readString();
+    }
+
+    public static final Creator<Artist> CREATOR = new Creator<Artist>() {
+        @Override
+        public Artist createFromParcel(Parcel in) {
+            return new Artist(in);
+        }
+
+        @Override
+        public Artist[] newArray(int size) {
+            return new Artist[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -37,5 +58,17 @@ public class Artist  {
 
     public void setArtist_id(String artist_id) {
         this.artist_id = artist_id;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(image);
+        dest.writeString(artist_id);
     }
 }
--- app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/ui/PlaylistFragment.java
+++ app/src/main/java/dragon/bakuman/iu/spotifyscratchaudiostreammitch/ui/PlaylistFragment.java
@@ -1,27 +1,62 @@
 package dragon.bakuman.iu.spotifyscratchaudiostreammitch.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import dragon.bakuman.iu.spotifyscratchaudiostreammitch.R;
+import dragon.bakuman.iu.spotifyscratchaudiostreammitch.adapters.CategoryRecyclerAdapter;
+import dragon.bakuman.iu.spotifyscratchaudiostreammitch.adapters.PlaylistRecyclerAdapter;
+import dragon.bakuman.iu.spotifyscratchaudiostreammitch.models.Artist;
 
 
-public class PlaylistFragment extends Fragment
-{
+public class PlaylistFragment extends Fragment implements PlaylistRecyclerAdapter.IMediaSelector {
 
     private static final String TAG = "PlaylistFragment";
 
     // UI Components
-
+    private RecyclerView mRecyclerView;
 
 
     // Vars
+    private PlaylistRecyclerAdapter mAdapter;
+    private ArrayList<MediaMetadataCompat> mMediaList = new ArrayList<>();
+    private IMainActivity mIMainActivity;
+    private String mSelectedCategory;
+    private Artist mSelectedArtist;
+    private MediaMetadataCompat mSelectedMedia;
 
+    public static PlaylistFragment newInstance(String category, Artist artist) {
 
+        PlaylistFragment playlistFragment = new PlaylistFragment();
+        Bundle args = new Bundle();
+        args.putString("category", category);
+        args.putParcelable("artist", artist);
+        playlistFragment.setArguments(args);
+        return playlistFragment;
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mSelectedCategory = getArguments().getString("category");
+            mSelectedArtist = getArguments().getParcelable("artist"); //parcelable because it is a cutome objecta
+
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +64,41 @@ public class PlaylistFragment extends Fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initRecyclerView(view);
+    }
 
+    private void updateDataset(){
+
+        mIMainActivity.hideProgressBar();
+        mAdapter.notifyDataSetChanged();
+
+    }
+
+    private void initRecyclerView(View view) {
+
+        if (mRecyclerView == null) {
+
+            mRecyclerView = view.findViewById(R.id.recycler_view);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            mAdapter = new PlaylistRecyclerAdapter(getActivity(), mMediaList, this);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mIMainActivity = (IMainActivity) getActivity();
+    }
+
+
+    @Override
+    public void onMediaSelected(int position) {
+
+    }
 }
 
 
